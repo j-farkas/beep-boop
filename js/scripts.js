9c9265a901933c6aff7e04d62abb68d39d964fad
Jared Farkas <jaredmfarkas@gmail.com>
2019-01-25 10:09:59 -0800

    Add dealing

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,60 @@ function parNum(num){
   }
   return arr;
 }
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+function createDeck(){
+  var deck = [];
+  for(i=1;i<=52;i++)
+  {
+    var toPush = "";
+    if(i%13 === 1)
+    {
+      toPush+="Ace"
+    }else if(i%13 === 11)
+    {
+      toPush+="Jack"
+    }else if(i%13 === 12)
+    {
+      toPush+="Queen"
+    }else if(i%13 === 0)
+    {
+      toPush+="King"
+    }else{
+    toPush += i%13;
+    }
+    toPush += " of ";
+    if(i/13 <= 1)
+    {
+      toPush+="Spades";
+    }else if(i/13 <= 2)
+    {
+      toPush+="Hearts"
+    }else if(i/13 <=3 )
+    {
+      toPush+="Clubs";
+    }else{
+    toPush += "Diamonds";
+    }
+
+    deck.push(toPush);
+  }
+  return deck;
+}
 $(document).ready(function(){
+  var deck = createDeck();
+  shuffle(deck);
+  console.log(deck);
   function toList(arr){
     arr.forEach(function(ar){
       $(".bbout").append("<li>" + ar +"</li>");
@@ -32,42 +85,13 @@ $(document).ready(function(){
     event.preventDefault();
     });
     $(".deal").click(function(event){
-      var deck = [];
-      for(i=1;i<=52;i++)
-      {
-        var toPush = "";
-        if(i%13 === 1)
-        {
-          toPush+="Ace"
-        }else if(i%13 === 11)
-        {
-          toPush+="Jack"
-        }else if(i%13 === 12)
-        {
-          toPush+="Queen"
-        }else if(i%13 === 0)
-        {
-          toPush+="King"
-        }else{
-        toPush += i%13;
-        }
-        toPush += " of ";
-        if(i/13 <= 1)
-        {
-          toPush+="Spades";
-        }else if(i/13 <= 2)
-        {
-          toPush+="Hearts"
-        }else if(i/13 <=3 )
-        {
-          toPush+="Clubs";
-        }else{
-        toPush += "Diamonds";
-        }
-
-        console.log(toPush);
-        deck.push(toPush);
-      }
-      console.log(deck);
+      $(".deal").hide();
+      $(".hit").show();
+      $(".stand").show();
+      $(".dcards").append("<li>" + deck[0] + deck[1] +"</li>");
+      $(".mycards").append("<li>" + deck[2] + deck[3] +"</li>");
+      //Put the used cards in the bottom of the deck
+      deck = deck.slice(4,deck.length).concat(deck.slice(0,3));
+      
     });
   });
