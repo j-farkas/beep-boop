fb402edbd94e5137b0c3a4b85f0f7faf3eecc0d5
Jared Farkas <jaredmfarkas@gmail.com>
2019-01-25 09:15:02 -0800

    Modify handling behavior

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function parNum(num){
   arr = [];
   for(var i = 0;i<num;i++)
   {
-    if([3].includes(i))
+    if([3].includes(i).toString())
     {
       arr.push("I'm sorry, Dave. I'm Afraid I can't do that");
     }else if([2].includes(i)){
@@ -10,24 +10,26 @@ function parNum(num){
     }else if([1].includes(i)){
       arr.push("Beep!");
     }else{
+      if(i.length>1){
+        arr.push(i);
+      }else{
       arr.push(i);
+      }
     }
   }
   return arr;
 }
 $(document).ready(function(){
-  /*function toList(arr){
+  function toList(arr){
     arr.forEach(function(ar){
       $("ul").append("<li>" + ar +"</li>");
-    });*/
-  $("form#transportation_survey").submit(function(event){
-      var input = $("#num").val();
-      var input = parNum(parseInt(input));
-      debugger;
-      input = parNum(input);
-      console.log(input);
-    //  toList(input);
+    });
+  }
+  $("form#numshift").submit(function(event){
+    var input = $("#num").val();
+    input = parNum(parseInt(input));
     console.log(input);
+    toList(input);
     event.preventDefault();
     });
   });
