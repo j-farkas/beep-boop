f6cae0db9d2095d250449d36485eecb180169f74
Jared Farkas <jaredmfarkas@gmail.com>
2019-01-25 09:57:27 -0800

    Add deck

--- js/scripts.js
+++ js/scripts.js
@@ -20,15 +20,54 @@ function parNum(num){
 $(document).ready(function(){
   function toList(arr){
     arr.forEach(function(ar){
-      $("ul").append("<li>" + ar +"</li>");
+      $(".bbout").append("<li>" + ar +"</li>");
     });
   }
   $("form#numshift").submit(function(event){
-    $('ul').empty();
+    $('.bbout').empty();
     var input = $("#num").val();
     input = parNum(parseInt(input));
     console.log(input);
     toList(input);
     event.preventDefault();
+    });
+    $(".deal").click(function(event){
+      var deck = [];
+      for(i=1;i<=52;i++)
+      {
+        var toPush = "";
+        if(i%13 === 1)
+        {
+          toPush+="Ace"
+        }else if(i%13 === 11)
+        {
+          toPush+="Jack"
+        }else if(i%13 === 12)
+        {
+          toPush+="Queen"
+        }else if(i%13 === 0)
+        {
+          toPush+="King"
+        }else{
+        toPush += i%13;
+        }
+        toPush += " of ";
+        if(i/13 <= 1)
+        {
+          toPush+="Spades";
+        }else if(i/13 <= 2)
+        {
+          toPush+="Hearts"
+        }else if(i/13 <=3 )
+        {
+          toPush+="Clubs";
+        }else{
+        toPush += "Diamonds";
+        }
+
+        console.log(toPush);
+        deck.push(toPush);
+      }
+      console.log(deck);
     });
   });
